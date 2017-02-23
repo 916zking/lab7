c87947f915781a7d651bc289c2ff8591f95c1943
Zackary King <zdking@ucsd.edu>
2017-02-22 18:20:13 -0800

    Added for the analytics

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,27 @@ var projects = require('../projects.json');
 
 exports.view = function(req, res){
   	res.render('index', projects);
+  	projects["grid"] = false;
+
+
+  	/*$(".btn btn-default likeBtn").click( function(e) {
+		ga("send", "event", 'like', 'click');
+	});
+*/
+
 };
+
+exports.viewGrid = function(req, res){
+  	res.render('index', projects);
+  	projects["grid"] = true;
+
+
+  	/*$(".likeBtn").click( function(e) {
+		ga("send", "event", 'like', 'click');
+	});
+*/
+
+};
+
+
+
