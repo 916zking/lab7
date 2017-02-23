ebb8cb927e86b49ee001ce32c5aa1268d9277b14
Zackary King <zdking@ucsd.edu>
2017-02-22 18:22:34 -0800

    Changed the index

--- routes/index.js
+++ routes/index.js
@@ -5,26 +5,16 @@ var projects = require('../projects.json');
  */
 
 exports.view = function(req, res){
-  	res.render('index', projects);
   	projects["grid"] = false;
+  	res.render('index', projects);
 
-
-  	/*$(".btn btn-default likeBtn").click( function(e) {
-		ga("send", "event", 'like', 'click');
-	});
-*/
 
 };
 
 exports.viewGrid = function(req, res){
-  	res.render('index', projects);
   	projects["grid"] = true;
 
-
-  	/*$(".likeBtn").click( function(e) {
-		ga("send", "event", 'like', 'click');
-	});
-*/
+  	res.render('index', projects);
 
 };
 
